590698b360b90398ea712625872ffeb2870cb82c
nastya <nnvishnya@gmail.com>
2020-11-05 22:35:26 +0200

    Implement serializers

--- server/game/serializers.py
+++ server/game/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers, fields
 from game.models import Game, Board, Ship, Player
+import json
+import numpy as np
 
 
 class TestQueryParams(serializers.Serializer):
@@ -16,20 +18,50 @@ class ShipSerializer(serializers.ModelSerializer):
         fields = ['x', 'y', 'rows', 'cols', 'length', 'orientation']
 
 
-class PlayerSerializer(serializers.ModelSerializer):
-    board = serializers.JSONField(source='board.board')
-    shots = serializers.JSONField(source='board.shots')
-    ships = ShipSerializer(many=True, source='board.ship_set')
+class NDarrayField(serializers.Field):
+    def to_representation(self, value):
+        return json.dumps(value.tolist())
+
+    def to_internal_value(self, data):
+        data = json.loads(data)
+        return np.array(data)
+
+
+class YouSerializer(serializers.ModelSerializer):
+    board = NDarrayField(source='board.board')
+    shots = NDarrayField(source='board.shots')
 
     class Meta:
         model = Player
-        fields = ['username', 'board', 'shots', 'ships']
+        fields = ['username', 'board', 'shots', ]
+
+
+class OpponentSerializer(serializers.ModelSerializer):
+    shots = NDarrayField(source='board.shots_with_marked')
+
+    class Meta:
+        model = Player
+        fields = ['username', 'shots']
 
 
 class GameSerializer(serializers.ModelSerializer):
-    playerA = PlayerSerializer()
-    playerA = PlayerSerializer()
+    current = serializers.SerializerMethodField()
+    you = serializers.SerializerMethodField()
+    opponent = serializers.SerializerMethodField()
 
     class Meta:
         model = Game
-        fields = ['rows', 'cols', 'playerA', 'playerB']
+        fields = ['rows', 'cols', 'current', 'you', 'opponent']
+
+    def get_current(self, obj):
+        player = self._context.get("player")
+        return 'you' if obj.current == player else 'opponent'
+
+    def get_you(self, obj):
+        player = self._context.get("player")
+        return YouSerializer(player).data
+
+    def get_opponent(self, obj):
+        player = self._context.get("player")
+        opponent = obj.playerB if player == obj.playerA else obj.playerA
+        return OpponentSerializer(opponent).data
