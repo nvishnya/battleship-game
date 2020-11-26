bea02e00164ad6be878651af1c81795b3a97e620
nastya <nnvishnya@gmail.com>
2020-11-26 18:30:10 +0200

    Move styles to seperate files

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   devServer: {
     proxy: "http://localhost:8000"
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "@/assets/scss/main.scss";`
+      }
+    }
   }
 };
