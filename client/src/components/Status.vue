f49e3eba2bb1947ff44fc89a8fb29190ca34d87c
nastya <nnvishnya@gmail.com>
2020-11-26 22:44:54 +0200

    Create a component to display game's status

---
+++ client/src/components/Status.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="status-bar">It's {{ yourTurn ? "your" : "your opponent's" }} turn.</div>
+</template>
+
+<script>
+export default {
+  props: {
+    yourTurn: Boolean,
+    isOver: Boolean,
+  },
+};
+</script>
