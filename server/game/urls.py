3ff45e67be9368fc37feb659ee934928a8b3aff7
nastya <nnvishnya@gmail.com>
2021-02-12 20:23:50 +0200

    Remove new_game view

--- server/game/urls.py
+++ server/game/urls.py
@@ -1,7 +1,6 @@
 from django.urls.conf import path
-from game.views import get_random_board, new_game
+from game.views import get_random_board
 
 urlpatterns = [
     path('random-board/', get_random_board),
-    path('new-game/', new_game)
 ]
--- server/game/views.py
+++ server/game/views.py
@@ -1,8 +1,7 @@
-from game.serializers import TestQueryParams, ShipSerializer, GameSerializer
-from rest_framework.generics import RetrieveAPIView
+from game.serializers import TestQueryParams, ShipSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from game.models import Board, Ship, Game
+from game.models import Board, Ship
 
 
 @api_view(['GET'])
@@ -12,11 +11,3 @@ def get_random_board(request):
     ships = Board.generate_initial_board(qp.data['rows'], qp.data['cols'])
     data = map(lambda x: Ship(**x), ships)
     return Response(ShipSerializer(data, many=True).data)
-
-
-@api_view(['POST'])
-def new_game(request):
-    qp = TestQueryParams(data=request.data)
-    qp.is_valid(raise_exception=True)
-    game = Game.create(qp.data['rows'], qp.data['cols'])
-    return Response({"game_id": game.id})
