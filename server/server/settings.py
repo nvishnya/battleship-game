e81014ed0caf1c6aec7ead371966f835ba2a4cd5
nastya <nnvishnya@gmail.com>
2021-03-12 20:23:06 +0200

    Get SECRET_KEY and DEBUG from environment variables

--- server/server/settings.py
+++ server/server/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=^kgpa58w^oyzw7(b9f9ci&&rvvg*s$hc(!3#2gl0ial3on=jl'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['*']
 
