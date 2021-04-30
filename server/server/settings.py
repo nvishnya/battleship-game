e2c40bbf24c66a7100ca50350c6a317263278ac3
nastya <nnvishnya@gmail.com>
2021-04-30 22:52:46 +0300

    Set secret key

--- server/server/settings.py
+++ server/server/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'q-nn$jg8^0+c-v-tda0@uvbkm4agepv$(*g0@ml8cq4s(lbm$a')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
